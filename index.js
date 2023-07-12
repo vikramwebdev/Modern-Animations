6d0921b4986f2b925406ae4d6439c2aa8a030e06
Vikram Kumar <98356772+vikramwebdev@users.noreply.github.com>
2023-07-13 01:19:17 +0530

    added index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+var typed = new Typed('#mytext',{
+    strings : ['Javascript Animations','Javascript Animations 2nd time'],
+    typeSpeed : 200
+})
+
+AOS.init({
+    duration : 2000
+})
+
+
+
+
+
+//      Vikram Kumar
+//     vikramkumar2552003@gmail.com
